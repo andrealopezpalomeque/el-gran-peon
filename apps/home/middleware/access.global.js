1c5f0a2f3745e4973a2b9c278580acc6f6f8abca
Andrea López Palomeque <andrealopezpalomeque@gmail.com>
2026-02-05 19:07:14 -0300

    minor update

--- apps/home/middleware/access.global.js
+++ apps/home/middleware/access.global.js
@@ -1,8 +1,9 @@
 export default defineNuxtRouteMiddleware((to) => {
   // Public routes that don't need access guard
   const publicRoutes = ['/subscription', '/access']
+  const path = to.path.replace(/\/$/, '') || '/'
 
-  if (publicRoutes.includes(to.path)) {
+  if (publicRoutes.includes(path)) {
     return
   }
 
