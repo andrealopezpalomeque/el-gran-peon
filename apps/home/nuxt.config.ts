acdb2363d01f6eca2b1adca9d0f90496d9dcdda8
Andrea López Palomeque <andrealopezpalomeque@gmail.com>
2026-02-25 19:42:40 -0300

    removes access code from home

--- apps/home/nuxt.config.ts
+++ apps/home/nuxt.config.ts
@@ -48,7 +48,6 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      accessCode: process.env.NUXT_PUBLIC_ACCESS_CODE || '',
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3001',
       firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
