b579eed79e5fb6cdc3131151275c75cf796e7c47
Andrea López Palomeque <andrealopezpalomeque@gmail.com>
2026-03-23 11:25:45 -0300

    copy change - email

--- apps/home/components/SiteFooter.vue
+++ apps/home/components/SiteFooter.vue
@@ -121,7 +121,7 @@
               <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="square" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
-              elgranpeon98@gmail.com
+              granpeon98@gmail.com
             </a>
           </div>
         </div>
